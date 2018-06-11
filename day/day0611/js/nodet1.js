04a63b0b572a82199054c54f30a3223871cf0ea7
imtangry <tang.rui,yu@foxmial.com>
2018-06-11 16:51:50 +0800

    add

--- day/day0611/js/nodet1.js
+++ day/day0611/js/nodet1.js
@@ -1,1 +1,11 @@
-console.log("测试node");
+// console.log("测试node");
+
+//加载文件模块
+var fs = require('fs');
+fs.readFile('test',function (err,data) {
+if(err){
+    console.log("读取错误");
+}else {
+    console.log(data.toString());
+}
+})
